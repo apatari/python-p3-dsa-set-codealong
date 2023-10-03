a5fc377068559cf92e37178ec1ff21501b504f59
Andy Patari <andy.patari@gmail.com>
2023-10-03 09:43:29 -0400

    passing bonus

--- lib/MySet.py
+++ lib/MySet.py
@@ -18,5 +18,32 @@ class MySet:
     
     def size(self):
         return len(self.dictionary)
+    
+    def __str__(self):
+        set_string = "MySet: {"
+        for key in self.dictionary:
+            set_string += f'{key},'
+        
+        set_string = set_string[:-1]
+        set_string += "}"
+        if self.size() == 0:
+            return "MySet: {}"
+
+        return set_string
+    
+    def clear(self):
+        self.dictionary = {}
+        return self.dictionary
+
+    
+test_set = MySet([1,2,3])
+
+print(test_set)
+
+test_set.clear()
+
+print(test_set.size())
+
+print(test_set)
 
     
